628ddfc209483266268b72c574eee5417d2ce4dc
wwei5743 <wwei5743@gmail.com>
2019-11-11 16:17:21 -0500

    Bug fixes; constants saved in utils.py

--- utils.py
+++ utils.py
@@ -11,15 +11,21 @@ import numpy as np
 import pyhash
 import math
 import random
+import time
 
 DATASET_URL = 'http://mattmahoney.net/dc/text8.zip'
 MYDIR = os.path.dirname(os.path.realpath(__file__))
 REJ_THRESHOLD = 1e-4
 HASHING_UB = 2000000
+EMBEDDING_SIZE = 300
+BATCH_SIZE = 128
+EPOCHS = 5
+WINDOW_SIZE = 5
 hasher = pyhash.fnv1a_32()
 myhasher = lambda x : hasher(x) % HASHING_UB
 LB_NGRAM = 3
 UB_NGRAM = 6
+random.seed(time.clock())
 
 def download_dataset_tokenize(url):
     print('Beginning file download from {}'.format(url))
@@ -65,18 +71,19 @@ def generate_subwords():
         subwords = set()
         word = '<{}>'.format(word)
         if len(word) <= LB_NGRAM:
-            return list(myhasher(word))
-        subwords.add(myhasher(word))
-        for window in range(LB_NGRAM, UB_NGRAM + 1):
-            for index in range(len(word) - window + 1):
-                subwords.add(myhasher(word[index: index+window+1]))
-        yield list(subwords)
+            yield [myhasher(word)]
+        else:
+            subwords.add(myhasher(word))
+            for window in range(LB_NGRAM, UB_NGRAM + 1):
+                for index in range(len(word) - window + 1):
+                    subwords.add(myhasher(word[index: index+window]))
+            yield list(subwords)
 
 def generate_batches(words_int, int_to_words, batch_size, window_size):
     subword_generator = generate_subwords()
     next(subword_generator)
-    for index in range(math.ceil(len(words_int) / batch_size) + 1):
-        batch = words_int[index * batch_size: (index + 1) * batch_size]
+    for index in range(math.ceil(len(words_int) / batch_size)):
+        batch = words_int[index * batch_size: min((index + 1) * batch_size, len(words_int))]
         input = list()
         target = list()
         for curr_index in batch:
@@ -92,3 +99,15 @@ def generate_batches(words_int, int_to_words, batch_size, window_size):
                 target.extend(neighbors)
         yield input, target
 
+# data = download_dataset_tokenize(DATASET_URL)
+# data = data[:5]
+# print(data)
+# freq = Counter(data)
+# freq = freq.most_common(len(freq))
+# words_to_int = {word[0]: index for index, word in enumerate(freq)}
+# int_to_words = {index: word for word, index in words_to_int.items()}
+# words_int = [words_to_int[word] for word in data]
+# batches = generate_batches(words_int, int_to_words, 5, 3)
+# for input, target in batches:
+#     for x, y in zip(input, target):
+#         print(str(x), int_to_words[y])
