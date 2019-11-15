cbb63495aec3449aa475889fa2afc25b0329e2f9
wwei5743 <wwei5743@gmail.com>
2019-11-15 09:44:20 -0500

    Average summed word vectors for OOV words

--- rw_test.py
+++ rw_test.py
@@ -1,8 +1,9 @@
 import pickle as pkl
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-from utils import generate_subwords
-from scipy import stats
+from utils import *
+data = download_dataset_tokenize(DATASET_URL)
+words_to_int, int_to_words, train_set = text_preprocessing(data)
 #Initialize subword generator
 gen = generate_subwords()
 next(gen)   
@@ -13,20 +14,24 @@ with open('rw.txt', 'r') as fp:
     for line in fp:
         line = line.split()
         rw_pairs.append([line[0], line[1]])
-        mean = np.mean([float(elem) for elem in line[2:]])
-        human_similarity.append(mean)
+        human_similarity.append(float(line[2]))
 model = pkl.load(open('embeddings.pkl','rb'))
 fastText_similarity = list()
 #For each word in the pair, generate subwords, sum up the subword vectors to perform cosine similarities
 for x1, x2 in rw_pairs:
-   x1_subwords = gen.send(x1) 
-   print(x1_subwords)
-   next(gen)
-   x2_subwords = gen.send(x2) 
-   next(gen)
-   x1_vector = np.sum(model[x1_subwords, :], axis=0).reshape(1,300)
-   x2_vector = np.sum(model[x2_subwords, :], axis=0).reshape(1,300)
-   similarity = cosine_similarity(x1_vector, x2_vector)
-   fastText_similarity.append(similarity[0][0])
+    x1_subwords = gen.send(x1) 
+    next(gen)
+    x2_subwords = gen.send(x2) 
+    next(gen)
+    if x1 not in words_to_int:
+        x1_vector = np.sum(model[x1_subwords, :], axis=0).reshape(1,300) / len(x1_subwords)
+    else:
+        x1_vector = np.sum(model[x1_subwords, :], axis=0).reshape(1,300)
+    if x2 not in words_to_int:
+        x2_vector = np.sum(model[x2_subwords, :], axis=0).reshape(1,300) / len(x2_subwords)
+    else:
+        x2_vector = np.sum(model[x2_subwords, :], axis=0).reshape(1,300)
+    similarity = cosine_similarity(x1_vector, x2_vector)
+    fastText_similarity.append(similarity[0][0])
 #Find spearman rank coeff between human similarity and fastText cosine similarity
-print(stats.spearmanr(human_similarity, fastText_similarity))
+print(spearman(torch.FloatTensor(fastText_similarity), torch.FloatTensor(human_similarity)))
--- ws353_test.py
+++ ws353_test.py
@@ -1,9 +1,10 @@
 import pickle as pkl
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-from utils import generate_subwords
-from scipy import stats
+from utils import *
 import csv
+data = download_dataset_tokenize(DATASET_URL)
+words_to_int, int_to_words, train_set = text_preprocessing(data)
 gen = generate_subwords()
 next(gen)   
 ws353_pairs = list()
@@ -20,12 +21,18 @@ with open('ws353.csv', 'r') as fp:
 model = pkl.load(open('embeddings.pkl','rb'))
 fastText_similarity = list()
 for x1, x2 in ws353_pairs:
-   x1_subwords = gen.send(x1) 
-   next(gen)
-   x2_subwords = gen.send(x2) 
-   next(gen)
-   x1_vector = np.sum(model[x1_subwords, :], axis=0).reshape(1,300)
-   x2_vector = np.sum(model[x2_subwords, :], axis=0).reshape(1,300)
-   similarity = cosine_similarity(x1_vector, x2_vector)
-   fastText_similarity.append(similarity[0][0])
-print(stats.spearmanr(human_similarity, fastText_similarity))
+    x1_subwords = gen.send(x1) 
+    next(gen)
+    x2_subwords = gen.send(x2) 
+    next(gen)
+    if x1 not in words_to_int:
+        x1_vector = np.sum(model[x1_subwords, :], axis=0).reshape(1,300) / len(x1_subwords)
+    else:
+        x1_vector = np.sum(model[x1_subwords, :], axis=0).reshape(1,300)
+    if x2 not in words_to_int:
+        x2_vector = np.sum(model[x2_subwords, :], axis=0).reshape(1,300) / len(x2_subwords)
+    else:
+        x2_vector = np.sum(model[x2_subwords, :], axis=0).reshape(1,300)
+    similarity = cosine_similarity(x1_vector, x2_vector)
+    fastText_similarity.append(similarity[0][0])
+print(spearman(torch.FloatTensor(human_similarity), torch.FloatTensor(fastText_similarity)))
